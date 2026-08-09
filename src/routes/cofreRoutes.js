2f9bf8d46e03b50b091f0efac5976ab7bc5a2a6a
RuanF2 <ruanfrancelino47@gmail.com>
2026-08-09 09:15:21 -0300

    Ajustes

--- src/routes/cofreRoutes.js
+++ src/routes/cofreRoutes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const verificarToken = require('../middlewares/verificarToken');
 const { criarSenha, listarSenhas, revelarSenha, excluirSenha } = require('../controllers/cofreController');
 
-// Todas as rotas do cofre exigem estar logado (token válido)
 router.post('/', verificarToken, criarSenha);
 router.get('/', verificarToken, listarSenhas);
 router.get('/:id/revelar', verificarToken, revelarSenha);
